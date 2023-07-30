995e21fabf5be69c8ab7fa53cba8bf819c33f0f6
Mirko Raner <mirko@raner.ws>
2023-07-30 10:48:00 -0700

    refactored IndexedIterator to more idiomatic style

--- src/main/kotlin/my/bookmaker/utilities/IndexedIterator.kt
+++ src/main/kotlin/my/bookmaker/utilities/IndexedIterator.kt
@@ -3,21 +3,13 @@ package my.bookmaker.utilities
 import java.util.function.Function
 import java.util.function.BiFunction
 
-class IndexedIterator<COLLECTION, ELEMENT>: Iterator<ELEMENT>
-{
+class IndexedIterator<COLLECTION, ELEMENT>(
+        private val collection: COLLECTION,
+        counter: Function<COLLECTION, Int>,
+        private val accessor: BiFunction<COLLECTION, Int, ELEMENT>):
+        Iterator<ELEMENT> {
     private var index: Int = 0
-    private val size: Int
-    private val collection: COLLECTION
-    private val accessor: BiFunction<COLLECTION, Int, ELEMENT>
-
-    constructor(collection: COLLECTION, counter: Function<COLLECTION, Int>, accessor: BiFunction<COLLECTION, Int, ELEMENT>)
-    {
-        this.collection = collection
-        this.accessor = accessor
-        size = counter.apply(collection)
-    }
-
+    private val size: Int = counter.apply(collection)
     override fun hasNext(): Boolean = index < size
-
     override fun next(): ELEMENT = accessor.apply(collection, index++)
 }
